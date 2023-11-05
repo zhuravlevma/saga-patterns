800f63432ae3ff5aa7e030a413cc1f7d82c92443
zhuravlevma <monge.gaspard.1746@gmail.com>
2023-11-05 20:55:18 +0300

    add report to process service

--- epic-saga/__process-service__/src/accounting.client.ts
+++ epic-saga/__process-service__/src/accounting.client.ts
@@ -4,8 +4,15 @@ import axios from 'axios';
 @Injectable()
 export class AccountingClient {
   createEmptyReport(orderId: string) {
-    return axios.patch(`localhost:3000/reports`, {
+    return axios.post(`localhost:3000/reports`, {
       orderId,
+    });
+  }
+
+  updateReportStatus(reportId: string, orderId: string, isValid: boolean) {
+    return axios.patch(`localhost:3000/reports/${reportId}`, {
+      orderId,
+      isValid,
     });
   }
 }
--- epic-saga/__process-service__/src/app.module.ts
+++ epic-saga/__process-service__/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { WarehouseController } from './warehouse.controller';
 import { WarehouseClient } from './warehouse.client';
 import { AccountingClient } from './accounting.client';
+import { DeliveryClient } from './delivery.client';
 
 @Module({
   imports: [],
   controllers: [WarehouseController],
-  providers: [WarehouseClient, AccountingClient],
+  providers: [WarehouseClient, AccountingClient, DeliveryClient],
 })
 export class AppModule {}
